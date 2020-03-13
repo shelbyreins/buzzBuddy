70eac52bb03582b08bd07b9a3ed72257d6ff1568
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-03-13 15:28:35 -0700

    Make calednarpage responsive and update index.html to fix error when deployed

--- client/src/components/Resource/index.js
+++ client/src/components/Resource/index.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react'
-// import { Link } from 'react-router-dom'
 // import "./style.css"
-// import Navbar from '../Navbar'
 
 
 class Resources extends Component {
     render() {
         return (
             <div>
-                {/* <Navbar /> */}
+                
             </div>
 
 
