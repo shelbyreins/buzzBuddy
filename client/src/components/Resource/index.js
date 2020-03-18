82aca80ac97481675aa1568ffeaf89c20a3e4808
Shelby Reinsimar <shelbyreins@yahoo.com>
2020-03-17 22:27:20 -0700

    Change axios get calls to post

--- client/src/components/Resource/index.js
+++ client/src/components/Resource/index.js
@@ -42,7 +42,7 @@ class Resources extends Component {
                 <div class="container">
                     <div class="row">
                         <div className="col-md-4">
-                        <button type="button" className="btn btn-lg btn-primary resourceBtn" onClick={this.clicked}>Videos</button>
+                        <button type="button" className="btn btn-lg btn-primary resourceBtn" onClick={this.clicked}>VIDEOS</button>
 
                         </div>
                         <div className="col-md-4">
